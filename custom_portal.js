d97d517c52b56d1f3a64bf8141cd0292e2fe8cbb
niemes <kevin@niemes.info>
2024-05-05 17:58:23 +0000

    add local app icons + new tab opening

--- custom_portal.js
+++ custom_portal.js
@@ -9,25 +9,71 @@
  custom stuff
 ===============================================================================
 */
+const config = {
+    customIcons: true,
+    newTabLink: true,
+}
 
-/*
- * Monkeypatch init_portal to customize the app tile style
- *
+async function addAppsLogo() {
+    const allApps = document.getElementsByClassName('app-tile')
+
+    for (let index = 0; index < allApps.length; index++) {
+        let app = allApps[index]
+        let name = app.getAttribute('data-appname')
+
+        if (config.newTabLink) {
+            app.dataset.link = allApps[index].href;
+            app.href = "";
+
+            app.addEventListener("click", (event) => {
+                let link = event.currentTarget.dataset.link;
+                console.log(link);
+                if (link) window.openApp(link)
+            });
+        }
+
+        const img = document.createElement('img')
+        img.className = 'appLogo'
+        imgUrl = `${app.baseURI.replace('#', '')}assets/themes/Nature-Mount/pictures/${name}.png`
+        
+        // Check if the URL of the logo exist.
+        await fetch(imgUrl)
+            .then(response => {
+                if (response.ok) {
+                    img.src = imgUrl;
+                    if (app.firstElementChild) {
+                        app.replaceChild(img, app.firstElementChild);
+                    } else {
+                        app.appendChild(img);
+                    }
+                } else {
+                    console.warn(`Invalid URL: ${imgUrl}`);
+                }
+            })
+            .catch(error => {
+                console.warn(`Error fetching URL: ${imgUrl}`, error);
+            });
+    }
+}
+
 init_portal_original = init_portal;
-init_portal = function()
+init_portal = async function()
 {
     init_portal_original();
-    // Some stuff here
+
+    window.openApp = (link) => window.open(link, "_blank");
+    if (config.customIcons) await addAppsLogo()
 }
-*/
 
 /*
  * Monkey patching example to do custom stuff when loading inside an app
  *
+ * */
+
 init_portal_button_and_overlay_original = init_portal_button_and_overlay;
 init_portal_button_and_overlay = function()
 {
     init_portal_button_and_overlay_original();
     // Custom stuff to do when loading inside an app
 }
-*/
+
